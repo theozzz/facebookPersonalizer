cbece83f60572d117285e2397d1ee7a9888f2c11
Theo <thcha@smile.fr>
2015-06-03 17:41:34 +0200

    adding rule to override the header ico

--- content.js
+++ content.js
@@ -9,7 +9,7 @@
       $('#blueBarNAXAnchor').css({'background' : 'orange'}); //navigation bar
       $('._5vb_, #contentCol').css({'background-color' : 'red'}); // main container
       $('._1ayn').css({'color' : 'black'}); //color font nav bar
-      $('#u_0_3').children(".jewelButton").css({'background-image': 'url(http://img11.hostingpics.net/pics/622712testfb.png)'}); // Change MSG ICO
+      $(".jewelButton").css({'background-image': 'url(http://img11.hostingpics.net/pics/622712testfb.png)'}); //CHANGE HEADER ICO
       //$('._5vb_').css({'background-color': 'red'}); //side of  the main container
       //$('#contentCol').css({'background-color': 'red'}); //middle of the main container
     }
